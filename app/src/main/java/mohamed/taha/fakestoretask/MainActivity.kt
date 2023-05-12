30cc549340ffe89b5c45e39360eb24ba0e1a7db9
mohamed.taha <mohammed.taha@shgardi.app>
2023-05-12 21:47:38 +0300

    add main activity

---
+++ app/src/main/java/mohamed/taha/fakestoretask/MainActivity.kt
@@ -0,0 +1,11 @@
+package mohamed.taha.fakestoretask
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+
+
+}
