3b5e1640f55cabb693bcb777286480ce169e6906
mohamed.taha <mohammed.taha@shgardi.app>
2023-05-12 22:20:21 +0300

    create home view model

---
+++ app/src/main/java/mohamed/taha/fakestoretask/ui/home/HomeViewModel.kt
@@ -0,0 +1,49 @@
+package mohamed.taha.fakestoretask.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import mohamed.taha.fakestoretask.data.Product
+import mohamed.taha.fakestoretask.data.ProductListCategory
+import mohamed.taha.fakestoretask.data.repository.ProductsRepository
+import javax.inject.Inject
+
+data class HomeScreenState(
+    var isLoading: Boolean = false,
+    var products: List<Product> = emptyList(),
+    var productListCategory: ProductListCategory = ProductListCategory.AllCategories
+)
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(private val repository: ProductsRepository) : ViewModel() {
+
+    private val _homeScreenState = MutableStateFlow(HomeScreenState())
+    val homeScreenState get() = _homeScreenState.asStateFlow()
+
+    fun getProducts(productListCategory: ProductListCategory) {
+        repository.getProducts(viewModelScope, productListCategory) { products ->
+            viewModelScope.launch {
+                _homeScreenState.value = homeScreenState.value.copy(
+                    products = products,
+                    isLoading = false,
+                    productListCategory = productListCategory
+                )
+            }
+        }
+    }
+
+    fun updateProduct(
+        product: Product,
+        productListCategory: ProductListCategory
+    ) = repository.updateProduct(viewModelScope, product) {
+        getProducts(productListCategory = productListCategory)
+    }
+
+    init {
+        _homeScreenState.value = homeScreenState.value.copy(isLoading = true)
+        getProducts(productListCategory = ProductListCategory.AllCategories)
+    }
+}
