cede4170be1cb385ad4339eda6d34df884359b71
mohamed.taha <mohammed.taha@shgardi.app>
2023-05-12 22:24:49 +0300

    create favorite screen

---
+++ app/src/main/java/mohamed/taha/fakestoretask/ui/favorites/FavoritesScreen.kt
@@ -0,0 +1,114 @@
+package mohamed.taha.fakestoretask.ui.favorites
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.germainkevin.collapsingtopbar.rememberCollapsingTopBarScrollBehavior
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import mohamed.taha.fakestoretask.ui.drawer.LeftDrawer
+import mohamed.taha.fakestoretask.ui.favorites.components.FavoriteProductItem
+import mohamed.taha.fakestoretask.ui.favorites.components.FavoritesTopAppBar
+import mohamed.taha.fakestoretask.utils.NavActions
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun FavoritesScreen(
+    currentRoute: String,
+    navActions: NavActions,
+    coroutineScope: CoroutineScope,
+    favoritesViewModel: FavoritesViewModel = hiltViewModel()
+) {
+    val scaffoldState = rememberScaffoldState()
+    val openLeftDrawer: () -> Unit = { coroutineScope.launch { scaffoldState.drawerState.open() } }
+    val closeLeftDrawer: () -> Unit =
+        { coroutineScope.launch { scaffoldState.drawerState.close() } }
+
+    val scrollBehavior = rememberCollapsingTopBarScrollBehavior(
+        centeredTitleAndSubtitle = false,
+        expandedTopBarMaxHeight = 126.dp
+    )
+
+    val addedAsFavoriteProducts =
+        favoritesViewModel.favoritesScreenState.value.allProducts.filter { it.addedAsFavorite }
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        scaffoldState = scaffoldState,
+        topBar = {
+            FavoritesTopAppBar(
+                favoriteItems = addedAsFavoriteProducts,
+                openLeftDrawer,
+                scrollBehavior,
+            )
+        },
+        drawerContent = {
+            LeftDrawer(
+                products = favoritesViewModel.favoritesScreenState.value.allProducts,
+                currentRoute,
+                navActions,
+                closeLeftDrawer
+            )
+        },
+        content = { contentPadding ->
+            if (addedAsFavoriteProducts.isEmpty()) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = MaterialTheme.colorScheme.background)
+                        .verticalScroll(rememberScrollState())
+                        .padding(contentPadding),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.no_cart_products),
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            } else {
+                LazyColumn(
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.background)
+                        .fillMaxSize()
+                        .padding(contentPadding)
+                ) {
+                    item {
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                    items(
+                        items = addedAsFavoriteProducts,
+                        key = { it.hashCode() }
+                    ) { product ->
+                        FavoriteProductItem(
+                            modifier = Modifier
+                                .padding(start = 4.dp, end = 4.dp, top = 4.dp, bottom = 12.dp)
+                                .fillMaxWidth()
+                                .animateItemPlacement(),
+                            product = product,
+                            removeFromFavorites = {
+                                favoritesViewModel.updateProduct(it)
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    )
+}
