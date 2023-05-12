79632e261a89d225ef0354c178c1771523c0d06f
mohamed.taha <mohammed.taha@shgardi.app>
2023-05-12 21:57:17 +0300

    fix setting gradlke

--- app/src/main/java/mohamed/taha/fakestoretask/MainActivity.kt
+++ app/src/main/java/mohamed/taha/fakestoretask/MainActivity.kt
@@ -3,7 +3,6 @@ package mohamed.taha.fakestoretask
 import androidx.activity.ComponentActivity
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
 
