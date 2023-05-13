a4becd392015cc44a3a4f70d491926ab348f35e6
mohamed.taha <mohammed.taha@shgardi.app>
2023-05-13 15:53:07 +0300

    fix total cost using constarint layout

--- app/src/main/java/mohamed/taha/fakestoretask/ui/cart/CartScreen.kt
+++ app/src/main/java/mohamed/taha/fakestoretask/ui/cart/CartScreen.kt
@@ -14,20 +14,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.germainkevin.collapsingtopbar.rememberCollapsingTopBarScrollBehavior
-import mohamed.taha.fakestoretask.ui.cart.components.CartTopAppBar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import mohamed.taha.fakestoretask.R
 import mohamed.taha.fakestoretask.ui.cart.components.CartProductItem
+import mohamed.taha.fakestoretask.ui.cart.components.CartTopAppBar
 import mohamed.taha.fakestoretask.ui.drawer.LeftDrawer
 import mohamed.taha.fakestoretask.utils.NavActions
-import mohamed.taha.fakestoretask.R
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -70,77 +72,89 @@ fun CartScreen(
         },
         content = { contentPadding ->
 
-            Column() {
-                if (addedToCartProducts.isEmpty()) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(color = MaterialTheme.colorScheme.background)
-                            .verticalScroll(rememberScrollState())
-                            .padding(contentPadding),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.no_cart_products),
-                            color = MaterialTheme.colorScheme.onBackground
-                        )
-                    }
-                } else {
-                    LazyColumn(
-                        modifier = Modifier
-                            .background(MaterialTheme.colorScheme.background)
-                            .wrapContentHeight()
-                            .padding(contentPadding)
-                    ) {
-                        item {
-                            Spacer(modifier = Modifier.height(8.dp))
-                        }
-                        items(
-                            items = addedToCartProducts,
-                            key = { it.hashCode() }
-                        ) { product ->
-                            CartProductItem(
-                                modifier = Modifier
-                                    .padding(start = 4.dp, end = 4.dp, top = 4.dp, bottom = 12.dp)
-                                    .fillMaxWidth()
-                                    .animateItemPlacement(),
-                                product = product,
-                                removeFromCart = {
-                                    cartViewModel.updateProduct(it)
-                                }
+            ConstraintLayout(modifier = Modifier.fillMaxHeight().fillMaxWidth()) {
+                val (list, total) = createRefs()
+
+                Column(modifier = Modifier.constrainAs(list) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(total.top, margin = 20.dp)
+                }) {
+                    if (addedToCartProducts.isEmpty()) {
+                        Column(
+                            modifier = Modifier
+                                .wrapContentWidth()
+                                .background(color = MaterialTheme.colorScheme.background)
+                                .verticalScroll(rememberScrollState())
+                                .padding(contentPadding),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.no_cart_products),
+                                color = MaterialTheme.colorScheme.onBackground
                             )
+                        }
+                    } else {
+                        LazyColumn(
+                            modifier = Modifier
+                                .background(MaterialTheme.colorScheme.background)
+                                .wrapContentHeight()
+                                .padding(contentPadding)
+                        ) {
+                            item {
+                                Spacer(modifier = Modifier.height(8.dp))
+                            }
+                            items(
+                                items = addedToCartProducts,
+                                key = { it.hashCode() }
+                            ) { product ->
+                                CartProductItem(
+                                    modifier = Modifier
+                                        .padding(
+                                            start = 4.dp,
+                                            end = 4.dp,
+                                            top = 4.dp,
+                                            bottom = 12.dp
+                                        )
+                                        .fillMaxWidth()
+                                        .animateItemPlacement(),
+                                    product = product,
+                                    removeFromCart = {
+                                        cartViewModel.updateProduct(it)
+                                    }
+                                )
+                            }
                         }
                     }
                 }
 
-                Row(
 
+
+                if (addedToCartProducts.isNotEmpty())
+                Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
+                        .background(Color.White)
+                        .constrainAs(total) {
+                            bottom.linkTo(parent.bottom, margin = 25.dp)
+                        },
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
-                        style = MaterialTheme.typography.headlineLarge.copy(
-                            color = MaterialTheme.colorScheme.onBackground,
-                        ),                        fontSize = 24.sp,
-                        text = "Total cost"
+                        modifier = Modifier.padding(10.dp),
+                        fontSize = 24.sp,
+                        text = "Total cost",
+                        fontWeight = FontWeight.Bold
+
                     )
                     Text(
-                        style = MaterialTheme.typography.headlineLarge.copy(
-                            color = MaterialTheme.colorScheme.onBackground,
-                        ),
+                        modifier = Modifier.padding(10.dp),
                         fontSize = 24.sp,
-                        text = "100 $"
+                        text = "100 $",
+                        fontWeight = FontWeight.Bold
                     )
-
-
                 }
-
             }
-
-
         }
     )
 }
